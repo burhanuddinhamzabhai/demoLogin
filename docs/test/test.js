743b719db3394515923f18e16a885286ee8a4614
Burhanuddin Bariyawala <burhanuddinhamzabhai@gmail.com>
2021-08-11 20:54:49 +0530

    major changes made

--- docs/test/test.js
+++ docs/test/test.js
@@ -11913,7 +11913,8 @@
         },
         {
             "ITS_ID": "60429797",
-            "Full_Name": "Husain bhai  Saifuddin bhai Bhikhapurwala"
+            "Full_Name": "Husain bhai  Saifuddin bhai Bhikhapurwala",
+            "Admin": "True"
         },
         {
             "ITS_ID": "60429798",
@@ -14469,7 +14470,8 @@
         },
         {
             "ITS_ID": "60460011",
-            "Full_Name": "Aliasgar bhai  Najmuddin bhai Saherwala"
+            "Full_Name": "Aliasgar bhai  Najmuddin bhai Saherwala",
+            "Admin": "True"
         },
         {
             "ITS_ID": "60460012",
