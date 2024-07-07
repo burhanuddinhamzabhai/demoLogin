6e7b7e8f3353f3746a95a9186865fad898f2d5eb
Burhanuddin Bariyawala <40425154+burhanuddinhamzabhai@users.noreply.github.com>
2024-07-07 10:03:26 +0530

    Update ashara1446.js

--- docs/test/ashara1446.js
+++ docs/test/ashara1446.js
@@ -1,1 +1,16 @@
-
+[{
+		"ITS_ID": "60460544",
+		"Full_Name": "Burhan bhai Mulla Hamza bhai Bariyawala",
+		"Admin": "True",
+		"Active":"false"
+	},
+	{
+		"ITS_ID": "60429797",
+		"Full_Name": "Husain bhai  Saifuddin bhai Bhikhapurwala",
+		"Admin": "True"
+	},
+	{
+		"ITS_ID": "60460011",
+		"Full_Name": "Aliasgar bhai  Najmuddin bhai Saherwala",
+		"Admin": "True"
+	}]
