70e86e042fb86e4538dc31bc265ea709a2417f9c
Burhanuddin Bariyawala <40425154+burhanuddinhamzabhai@users.noreply.github.com>
2024-08-01 18:02:22 +0530

    Update ashara1446.js

--- docs/test/ashara1446.js
+++ docs/test/ashara1446.js
@@ -785,12 +785,12 @@
     "Admin": "false"
   },
   {
-    "ITS_ID": 20300231123,
+    "ITS_ID": 20300231,
     "Full_Name": "Kayam bhai Godhrawala",
     "Admin": "false"
   },
   {
-    "ITS_ID": 30425548123,
+    "ITS_ID": 30425548,
     "Full_Name": "Burhanuddin Murtaza Lakdawala",
     "Admin": "false"
   },
