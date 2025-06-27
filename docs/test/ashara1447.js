5344609bd51676d3ad4f75bd2e39b21f545ca2e6
Burhanuddin Bariyawala <burhanuddinhamzabhai@gmail.com>
2025-06-27 09:00:05 +0530

    new miqaat

---
+++ docs/test/ashara1447.js
@@ -0,0 +1,627 @@
+[
+  {
+    "ITS_ID": 60460723,
+    "Full_Name": "Sugraben Yusuf bhai Bodeliwala ",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 20401087,
+    "Full_Name": "Qaizarbhai Hatim bhai Calcuttawala",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 60443509,
+    "Full_Name": "Hafiza ben Aqeel bhaI Valikarimwala",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 60460250,
+    "Full_Name": "Zainab bem Fakhruddin Kalolwala",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 60460562,
+    "Full_Name": "Abbas bhai Qurbanhusain Lokhandwala",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 60460406,
+    "Full_Name": "Khadizaben Shuleman Bhuriwala",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 30460784,
+    "Full_Name": "Zainab ben Shk Akbarali Bhikhapurwala",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 60430782,
+    "Full_Name": "Moiz bhai Nooruddin bhai Molwala",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 60466862,
+    "Full_Name": "Fatema ben M Mohammed bhai Dhundhyawala ",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 20403299,
+    "Full_Name": "Husaina ben Aunali bhai Poonawala",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 20403461,
+    "Full_Name": "Tayyab bhai Fakhruddin bhai Nathodiwala",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 20429826,
+    "Full_Name": "Zahera ben Nasir bhai Jojwala",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 60466871,
+    "Full_Name": "Masuma ben Tehsin bhai Dalal",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 20429807,
+    "Full_Name": "Ismai bhai Kalimuddin bhai Lakhwala",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 60463430,
+    "Full_Name": "Sameem ben Nooruddin bhai Bakrolwala",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 50485665,
+    "Full_Name": "Abbas bhai Asgar bhai Piplodwala",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 20391703,
+    "Full_Name": "Shk Nooruddin bhai Abbas bhai Lakhwala",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 60460356,
+    "Full_Name": "Zainab ben Abbas bhai Poonawala",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 20401043,
+    "Full_Name": "Hajra ben Abbas bhai Saherwala",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 20424660,
+    "Full_Name": "Aziz bhai M Rehmatali Saranpurwala",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 20433907,
+    "Full_Name": "Taher bhai Saifuddin bhai Simaliyawala",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 60460520,
+    "Full_Name": "Sakina ben Moiz bhai Dalal",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 30708669,
+    "Full_Name": "Samir bhai Shabbir bhai Bhikapurwala",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 60463893,
+    "Full_Name": "Idres bhai Shk Sadiq bhai Lakdawala",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 20398106,
+    "Full_Name": "Anena ben Saifuddin bhai Dalal",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 30708668,
+    "Full_Name": "Shk Nooruddin bhai Shk Fakhruddin bhai Vaghjipurwala",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 50427236,
+    "Full_Name": "Hazra ben Shk Imran bhai Valikarimwala",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 60460085,
+    "Full_Name": "Sakina ben Fakhruddin bhai Pavaghadwala",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 60466218,
+    "Full_Name": "Mubena ben Nooruddin Poonawala",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 60429724,
+    "Full_Name": "Rasidaben Fakhruddin bhai Bootwala",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 30801827,
+    "Full_Name": "Zahera ben Hamza bhai Colombowala",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 60443532,
+    "Full_Name": "Farida ben Hasan bhai Ratanpurwala",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 30708531,
+    "Full_Name": "Tasneem ben Fidali bhai Antelawala",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 20400381,
+    "Full_Name": "Salma ben Habib bhai Limkhedawala",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 60436412,
+    "Full_Name": "Raziya ben Bakir bhai Electicwala",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 60443731,
+    "Full_Name": "Sakela ben Akbar bhai Vaghjipurwala",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 60430861,
+    "Full_Name": "Shabbir bhai Shk Johar bhai Electickwala",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 60460165,
+    "Full_Name": "Umema ben Johar bhai Dhundhiyawala",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 60463455,
+    "Full_Name": "Zainab ben Fakhruddin bhai Kakajiwala",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 60460828,
+    "Full_Name": "Saifuddin bhai Najmuddin bhai Lakdawala",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 60429514,
+    "Full_Name": "Rabab ben Karim bhai Karimi",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 20403275,
+    "Full_Name": "Hamza bhai M Hatim bhai Chatriwala",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 60463425,
+    "Full_Name": "Kubraben Ismail bhai Piplodwala",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 60463089,
+    "Full_Name": "Fayeka ben Habib bhai Antelawala ",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 60443980,
+    "Full_Name": "Nafisa ben Ismail bhai Limkhedawala",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 20424676,
+    "Full_Name": "Asgar bhai Aun bhai Goradawala",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 20403552,
+    "Full_Name": "Ismail bhai Kika bhai Paghdiwala",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 60443826,
+    "Full_Name": "Haider bhai Taiyab bhai Chatriwala",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 60460127,
+    "Full_Name": "Hasena ben Mohammed bhai Chandawala",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 20429452,
+    "Full_Name": "Baqir bhai Abbas bhai Electricwala",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 60463793,
+    "Full_Name": "Sakina ben Nooruddin bhai Jalalbhaiwala",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 60436945,
+    "Full_Name": "Ghulamali Sulemanji Vimanwala",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 60460640,
+    "Full_Name": "rehanaben mohammedbhai dala",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 20403204,
+    "Full_Name": "Zainab ben Shk Asgar bhai Muwadawala",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 20429823,
+    "Full_Name": "rizvanaben akbaribhai dalal",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 20377744,
+    "Full_Name": "taherbhai yusufali karimi",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 60430701,
+    "Full_Name": "salmaben sakirbhai kaknpurwala",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 30700680,
+    "Full_Name": "zehraben fakhruddi jalalbhaiwala",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 60460528,
+    "Full_Name": "rumana mubasshirbhai morawala",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 60460044,
+    "Full_Name": "mohammedbhai safakathusen lokhandwala",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 20403160,
+    "Full_Name": "najmuddin taherali bodeliwal",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 20403732,
+    "Full_Name": "Bilkes ben Qurbanhusain bhai Lakdawala",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 60443684,
+    "Full_Name": "Sakira ben Dosaji Kapadiya",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 20424626,
+    "Full_Name": "mohammedbhai onalibhai vahlabhai",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 60463420,
+    "Full_Name": "Rafika ben Shk Mohammed bhai Lehriwala",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 60430772,
+    "Full_Name": "Munira ben Mohammed bhai Ginwala",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 40434500,
+    "Full_Name": "Sayema ben Muslim bhai Barodawala",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 60460558,
+    "Full_Name": "Tasneem ben Mustafa bhai Gajipurwala",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 30487073,
+    "Full_Name": "Zainab ben Iqbal bhai Dalal",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 60460832,
+    "Full_Name": "Fatema ben Abdulhusain bhai Kapadia",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 20403929,
+    "Full_Name": "Fatema ben Abbasbhai Goradawala",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 20403958,
+    "Full_Name": "Fatema ben Akbar bhai Bodeliwala",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 30708494,
+    "Full_Name": "Rasida ben Mustansir bhai Rangoonwala",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 60463884,
+    "Full_Name": "Nemat ben Ibrahim bhai Lokhandwala",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 60460676,
+    "Full_Name": "Sakina ben Mansoor bhai Dalal",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 20403909,
+    "Full_Name": "Qutbuddin bhai Abdulhusain bhai Aabakutwala",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 60463487,
+    "Full_Name": "Samoil bhai Asgar bhai Khokawala",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 20403154,
+    "Full_Name": "Bilkes ben Safdar bhai Gajipurwala",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 20391729,
+    "Full_Name": "Karim bhai Akbar bhai Lehriwala",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 20403434,
+    "Full_Name": "Zainab ben Shk Akbar bhai Poonawala",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 60430830,
+    "Full_Name": "Zainab ben Nasir bhai Poonawala",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 60436845,
+    "Full_Name": "Husain bhai Moiz bhai Selvawala",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 6046093,
+    "Full_Name": "Tahera ben Mazahir bhai Piplodwala",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 20401015,
+    "Full_Name": "Mazahir bhai Shk Noman bhai Mithiborwala",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 20430507,
+    "Full_Name": "Asgar bhai Hakimuddin bhai Bagban",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 20391738,
+    "Full_Name": "Safiya ben Najmuddin bhai Vavkundliwala",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 20400337,
+    "Full_Name": "Vajiuddin bhai Zainuddin bhai Master",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 20391707,
+    "Full_Name": "Zakiuddin bhai Zainuddin bhai Master",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 20403767,
+    "Full_Name": "Fatema ben Moiz bhai Master",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 60463378,
+    "Full_Name": "Sabana ben Baqir bhai Selvawala",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 60443707,
+    "Full_Name": "Arwa ben Qutbuddin Bhai Vaghjipurwala",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 20403630,
+    "Full_Name": "Najmuddin bhai Sulemanji Kapadia",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 20424670,
+    "Full_Name": "Kezar bhai Saleh bhai Piplodwala",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 60429570,
+    "Full_Name": "Zareena ben Nooruddin bhai Vali",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 60443703,
+    "Full_Name": "Salma ben Shk Fakhruddin bhai Lokhandwala",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 60463282,
+    "Full_Name": "Fatema ben Ishaq bhai Eralwala",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 60443769,
+    "Full_Name": "Sakina ben Mohsin bhai Lokhandwala",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 20403270,
+    "Full_Name": "Saifuddin bhai Kalimuddin bhai Lakhwala",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 20398145,
+    "Full_Name": "Mohammed bhai Kalimuddin bhai Bhikapurwala",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 20400389,
+    "Full_Name": "Mariyam ben Asgar bhai Kapadiya",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 60460593,
+    "Full_Name": "Hatim bhai Nooruddin bhai Gajipurwala",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 20401050,
+    "Full_Name": "Mohammed bhai Fakhruddin bhai Gajipurwala",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 60443873,
+    "Full_Name": "Nooruddin bhai Fakhruddin bhai Gajipurwala",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 30913847,
+    "Full_Name": "Safiyah ben Taherali Lakdawala",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 30917568,
+    "Full_Name": "Qubra ben Fidaali Fidvi",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 60430824,
+    "Full_Name": "Qubra ben Hamza bhai Gulamhusainwala",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 50455339,
+    "Full_Name": "M Farda ben Shk Yusuf bhai Poonawala",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 20403962,
+    "Full_Name": "Ruqaiya ben Nuruddin bhai Nasikwala",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 20403958,
+    "Full_Name": "Fatema ben akbar bhai bodeliwala",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 20424659,
+    "Full_Name": "Nuruddin jalal bhaiwala",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 60430607,
+    "Full_Name": "Amenaben amdawadwala",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 20401056,
+    "Full_Name": "Hawaa ben poonawala",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 60436307,
+    "Full_Name": "Mariya chatriwala",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 20403178,
+    "Full_Name": "Ateka ben dalal",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 20403126,
+    "Full_Name": "Siraj bhai Bootwala",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 20403129,
+    "Full_Name": "Fatema adadrawala",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 60430863,
+    "Full_Name": "Salma bai Kapadia",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 30708528,
+    "Full_Name": "Salma fakhruddin Lakhwala",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 60466969,
+    "Full_Name": "Rizwana Ben Ajabshawala",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 60463196,
+    "Full_Name": "Zainab ben Murtaza Valikarimwala",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 30430386,
+    "Full_Name": "Sakina ben  Poonawala",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 20403108,
+    "Full_Name": "Munira ben Poonawala",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 60436269,
+    "Full_Name": "Zainab ben Barafwala",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 20398125,
+    "Full_Name": "Nuruddin bhai Pavagadhwala",
+    "Admin": false
+  },
+  {
+    "ITS_ID": 60429797,
+    "Full_Name": "Husain Shk.Saifuddin Bhikhapurwala",
+    "Admin": true
+  }
+]
